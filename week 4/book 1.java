082b7fd17f550fdc30ac21cdb88149c6eb087002
Shilpavaddara <shilpa.cs25@bmsce.ac.in>
2025-11-05 12:02:09 +0530

    Add files via upload

---
+++ week 4/book 1.java
@@ -0,0 +1,100 @@
+import java.util.Scanner;
+
+// Book class definition
+class Book {
+    private String name;
+    private String author;
+    private double price;
+    private int numPages;
+
+    // Constructor
+    public Book(String name, String author, double price, int numPages) {
+        this.name = name;
+        this.author = author;
+        this.price = price;
+        this.numPages = numPages;
+    }
+
+    // Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setNumPages(int numPages) {
+        this.numPages = numPages;
+    }
+
+    // Getters
+    public String getName() {
+        return name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getNumPages() {
+        return numPages;
+    }
+
+    // toString method
+    @Override
+    public String toString() {
+        return "Book Details:\n" +
+               "Name: " + name + "\n" +
+               "Author: " + author + "\n" +
+               "Price: ₹" + price + "\n" +
+               "Pages: " + numPages;
+    }
+}
+
+// Main class to create and display books
+class Bookstrore{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter number of books to create: ");
+        int n = scanner.nextInt();
+        scanner.nextLine(); // consume newline
+
+        Book[] books = new Book[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("\nEnter details for Book " + (i + 1) + ":");
+
+            System.out.print("Name: ");
+            String name = scanner.nextLine();
+
+            System.out.print("Author: ");
+            String author = scanner.nextLine();
+
+            System.out.print("Price: ");
+            double price = scanner.nextDouble();
+
+            System.out.print("Number of Pages: ");
+            int pages = scanner.nextInt();
+            scanner.nextLine(); // consume newline
+
+            books[i] = new Book(name, author, price, pages);
+        }
+
+        System.out.println("\n--- Book List ---");
+        for (int i = 0; i < n; i++) {
+            System.out.println("\nBook " + (i + 1) + ":\n" + books[i].toString());
+        }
+
+        scanner.close();
+    }
+}
